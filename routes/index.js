5992b06e0a416792d6beecd62231cc38fae95fa5
Muhammad Faizan <muhammad.faizan01@live.com>
2018-07-04 15:55:03 +0500

    FEAT: defined socket connection in template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const debug = require('debug')('chatter:routes:index');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    host: req.headers.host,
+    protocol: req.protocol
+  });
 });
 
 module.exports = router;
