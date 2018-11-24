9e8c7d94c3a2f07c50d1264044d8b58da4d0ff62
muhammadfaizan <muhammad.faizan01@live.com>
2018-11-25 02:22:13 +0500

    improved socket controller to have socket binded to each event handler, also added comment. Debug module is not working

--- controllers/socket.js
+++ controllers/socket.js
@@ -1,4 +1,5 @@
 const { io, setSocket } = require('../services/index').socket;
+const debug = require('debug')('chatter:controller:socket');
 class socketEvent {
     constructor(eventName, handler) {
         this.eventName = eventName;
@@ -7,17 +8,22 @@ class socketEvent {
 }
 
 const SocketController = {
+    /**
+     * @function registerEvents
+     * @description registers all the events and their handlers in one place
+     */
     registerEvents: (socket) => [
         new socketEvent('setName', SocketController.setName.bind(socket))
     ].map(evt => {
         socket.on(evt.eventName, evt.handler);
     }),
 
-    setName: (data) => {
+    setName: function (data) {
         console.log(data);
-        this.username = data.name;
+        // socket.username = data.name;
+        this.emit('acknowledged', { success: true });
     },
-    
+
     sendSocketToService: (socketIO) => {
         const socketInstance = setSocket(socketIO);
         SocketController.handleConnectionEvent(socketInstance.io);
