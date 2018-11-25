c6bc840508e179c415252c337257b25f6281bf30
muhammadfaizan <muhammad.faizan01@live.com>
2018-11-25 21:20:44 +0500

    handled username info. added event to handle own message

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,20 +2,34 @@ var App = (function () {
     function logAndAlert(data) {
         console.log(data);
     }
-    socket.on('acknowledged', function (data) {
-        if (data && data.success) {
-            window.location.href = "/chat"
+
+    socket.on('username', function (data) {
+        console.log(data);
+        if (!data.username) {
+            // redirect to homepage if user doesn't have a name
+            // window.location.href = "/"
         }
     });
+
+    socket.emit('clientInfo', {
+        data: ['username']
+    })
+    socket.on('username', function (data) {
+        console.log(data);
+    })
     return {
         onMessage: function (evt) {
             evt.preventDefault();
-            alert('message: ', evt.target.name.value);
-            if (evt.target.name && evt.target.name.value) {
 
-                // socket.emit('setName', {
-                //     name: evt.target.name.value
-                // });
+            if (evt.target.message && evt.target.message.value) {
+                let messageDiv = document.createElement("div");
+                messageDiv.className = "message own";
+                messageDiv.innerText = evt.target.message.value;
+                const roomDiv = document.getElementById('room');
+                roomDiv.appendChild(messageDiv);
+                roomDiv.scrollTop = roomDiv.scrollHeight;
+                evt.target.message.value = '';
+                evt.target.message.focus();
             }
             return false;
         }
