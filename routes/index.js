f592f4d48b98177549a806e101f1d79d6e68f2b4
muhammadfaizan <muhammad.faizan01@live.com>
2018-11-25 17:02:36 +0500

    added new route for chatroom

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,16 @@ var router = express.Router();
 const debug = require('debug')('chatter:routes:index');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', {
+    host: req.headers.host,
+    protocol: req.protocol
+  });
+});
+
+/* GET home page. */
+router.get('/chat', function (req, res, next) {
+  res.render('chat', {
     host: req.headers.host,
     protocol: req.protocol
   });
