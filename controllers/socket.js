e2a6a947471e0e2a40027aabfeead3072149a16e
muhammadfaizan <muhammad.faizan01@live.com>
2018-11-25 21:20:04 +0500

    created event handler to send client specific data

--- controllers/socket.js
+++ controllers/socket.js
@@ -1,5 +1,7 @@
 const { io, setSocket } = require('../services/index').socket;
 const debug = require('debug')('chatter:controller:socket');
+const R = require('ramda');
+
 class socketEvent {
     constructor(eventName, handler) {
         this.eventName = eventName;
@@ -8,17 +10,33 @@ class socketEvent {
 }
 
 const SocketController = {
+    ClientVisibleInfo: ['username'],
     /**
      * @function registerEvents
      * @description registers all the events and their handlers in one place
      */
     registerEvents: (socket) => [
-        new socketEvent('setName', SocketController.setName.bind(socket))
+        new socketEvent('setName', SocketController.setName.bind(socket)),
+        new socketEvent('clientInfo', SocketController.clientInformationHandler.bind(socket))
     ].map(evt => {
         socket.on(evt.eventName, evt.handler);
     }),
 
     setName: function (data) {
+        this.username = data.name;
+        this.emit('acknowledged', { success: true });
+    },
+
+    clientInformationHandler: function (requiredKeys) {
+        const keys = R.intersection(requiredKeys.data, SocketController.ClientVisibleInfo);
+        keys.forEach((key) => {
+            this.emit(key, {
+                [`${key}`]: this[key]
+            });
+        })
+    },
+
+    getName: function (data) {
         console.log(data);
         // socket.username = data.name;
         this.emit('acknowledged', { success: true });
